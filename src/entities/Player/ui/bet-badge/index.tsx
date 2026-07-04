7fb29cff363b3e4cf816696fb885c62937fb66ac
RogerIvan898 <Rogerpivan@yandex.ru>
2026-07-04 14:38:24 +0300

    feat: add chips sound

--- src/entities/Player/ui/bet-badge/index.tsx
+++ src/entities/Player/ui/bet-badge/index.tsx
@@ -1,5 +1,9 @@
+import React from 'react';
+
 import { cn } from 'shared/utils';
 import type { DirectionType } from 'shared/types/primitives';
+import { useSound } from 'shared/hooks/useSound';
+import chipsSound from 'assets/sounds/chips.mp3';
 
 import { ChipStack } from 'shared/ui/chip';
 
@@ -10,9 +14,22 @@ interface Props {
   position: DirectionType;
 }
 
-export const BetBadge = ({ amount, position }: Props) => (
-  <div className={cn(styles.betArea, styles[`bet_${position}`])}>
-    <ChipStack amount={amount} />
-    {amount}
-  </div>
-);
+export const BetBadge = ({ amount, position }: Props) => {
+  const { play: playChipsSound } = useSound(chipsSound);
+  const prevAmountRef = React.useRef(0);
+
+  React.useEffect(() => {
+    if (amount > prevAmountRef.current) {
+      playChipsSound();
+    }
+
+    prevAmountRef.current = amount;
+  }, [amount, playChipsSound]);
+
+  return (
+    <div className={cn(styles.betArea, styles[`bet_${position}`])}>
+      <ChipStack amount={amount} />
+      {amount}
+    </div>
+  );
+};
